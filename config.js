8e10c61bf0c4eb2cbd25cbdbdfc1e6ba2281c23c
admin <admin@myapp.com>
2021-06-26 19:57:14 +0200

    new infra project

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
-module.exports = () => ({
-  projectId: "aws-test",
+const pkg = require("./package.json");
+module.exports = ({ stage }) => ({
+  projectName: pkg.name,
+  keyPair: { name: "kp" },
+  eip: { name: "eip" },
+  ec2Instance: {
+    name: "web-server-ec2-example",
+    properties: () => ({
+      InstanceType: "t2.micro",
+    }),
+  },
 });
--- iac.js
+++ iac.js
@@ -2,22 +2,49 @@ const assert = require("assert");
 const { AwsProvider } = require("@grucloud/provider-aws");
 const hook = require("./hook");
 
-const createResources = async ({ provider, resources: {} }) => {
-  const { stage } = provider.config;
+exports.config = require("./config");
 
-  return {};
+const createResources = async ({ provider, resources: { keyPair } }) => {
+  const { config } = provider;
+  assert(config.eip);
+  const eip = await provider.makeElasticIpAddress({
+    name: config.eip.name,
+  });
+
+  const image = await provider.useImage({
+    name: "Amazon Linux 2",
+    properties: () => ({
+      Filters: [
+        {
+          Name: "architecture",
+          Values: ["x86_64"],
+        },
+        {
+          Name: "description",
+          Values: ["Amazon Linux 2 AMI *"],
+        },
+      ],
+    }),
+  });
+
+  return {
+    eip,
+    ec2Instance: await provider.makeEC2({
+      name: config.ec2Instance.name,
+      dependencies: { keyPair, eip, image },
+      properties: config.ec2Instance.properties,
+    }),
+  };
 };
 exports.createResources = createResources;
 
 exports.createStack = async () => {
+  // Create a AWS provider
   const provider = AwsProvider({ config: require("./config") });
-  const { stage } = provider.config;
-  assert(stage, "missing stage");
-
-  const resources = await createResources({
-    provider,
-    resources: {},
+  const keyPair = await provider.useKeyPair({
+    name: provider.config.keyPair.name,
   });
+  const resources = await createResources({ provider, resources: { keyPair } });
 
   return {
     provider,
