4ba795f084f39f4be6e69072215ff2600789309d
admin <admin@myapp.com>
2021-06-26 19:54:55 +0200

    new infra project

--- hook.js
+++ hook.js
@@ -1,97 +1,29 @@
 const assert = require("assert");
-const ping = require("ping");
-const path = require("path");
-const Client = require("ssh2").Client;
-const { retryCall } = require("@grucloud/core").Retry;
 
-const testPing = ({ host }) =>
-  ping.promise.probe(host, {
-    timeout: 10,
-  });
-
-//const privateKey = require("fs").readFileSync(
-//  path.resolve(__dirname, "../../../secrets/kp.pem")
-//);
-
-const testSsh = async ({ host, username = "ec2-user" }) =>
-  await new Promise((resolve, reject) => {
-    const conn = new Client();
-    conn
-      .on("ready", function () {
-        //console.log(`ssh to ${host} ok`);
-        conn.end();
-        resolve();
-      })
-      .on("error", function (error) {
-        // console.log(`cannot ssh to ${host}`, error);
-        reject(error);
-      })
-      .connect({
-        host,
-        port: 22,
-        username,
-        agent: process.env.SSH_AUTH_SOCK,
-        //privateKey,
-      });
-  });
-
-module.exports = ({ resources: { eip, ec2Instance }, provider }) => {
-  assert(provider);
+module.exports = ({ resources, provider }) => {
   return {
     onDeployed: {
       init: async () => {
-        const eipLive = await eip.getLive();
-        const serverLive = await ec2Instance.getLive();
-        assert(serverLive, "server should be alive");
-        //Static checks
-        assert.equal(serverLive.PublicIpAddress, eipLive.PublicIp);
-
-        const host = eipLive.PublicIp;
-        return {
-          host,
-        };
+        return {};
       },
       actions: [
-        //Cannot ping from CircleCI
-        /*{
-          name: "Ping",
-          command: async ({ host }) => {
-            const alive = await retryCall({
-              name: `ping ${host}`,
-              fn: async () => {
-                const { alive } = await testPing({ host });
-                if (!alive) {
-                  throw Error(`cannot ping ${host} yet`);
-                }
-                return alive;
-              },
-              shouldRetryOnException: () => true,
-              retryCount: 40,
-              retryDelay: 5e3,
-            });
-            assert(alive, `cannot ping ${host}`);
-          },
-        },*/
         {
-          name: "SSH",
-          command: async ({ host }) => {
-            await retryCall({
-              name: `ssh ${host}`,
-              fn: async () => {
-                await testSsh({ host });
-                return true;
-              },
-              shouldRetryOnException: () => true,
-              config: { retryCount: 40, retryDelay: 5e3 },
-            });
-          },
+          name: "TODO",
+          command: async ({}) => {},
         },
       ],
     },
+
     onDestroyed: {
-      init: () => {
-        //console.log("ec2 onDestroyed");
+      init: async () => {
+        return {};
       },
+      actions: [
+        {
+          name: "Perform check",
+          command: async ({}) => {},
+        },
+      ],
     },
   };
 };
--- iac.js
+++ iac.js
@@ -2,49 +2,22 @@ const assert = require("assert");
 const { AwsProvider } = require("@grucloud/provider-aws");
 const hook = require("./hook");
 
-exports.config = require("./config");
+const createResources = async ({ provider, resources: {} }) => {
+  const { stage } = provider.config;
 
-const createResources = async ({ provider, resources: { keyPair } }) => {
-  const { config } = provider;
-  assert(config.eip);
-  const eip = await provider.makeElasticIpAddress({
-    name: config.eip.name,
-  });
-
-  const image = await provider.useImage({
-    name: "Amazon Linux 2",
-    properties: () => ({
-      Filters: [
-        {
-          Name: "architecture",
-          Values: ["x86_64"],
-        },
-        {
-          Name: "description",
-          Values: ["Amazon Linux 2 AMI *"],
-        },
-      ],
-    }),
-  });
-
-  return {
-    eip,
-    ec2Instance: await provider.makeEC2({
-      name: config.ec2Instance.name,
-      dependencies: { keyPair, eip, image },
-      properties: config.ec2Instance.properties,
-    }),
-  };
+  return {};
 };
 exports.createResources = createResources;
 
 exports.createStack = async () => {
-  // Create a AWS provider
   const provider = AwsProvider({ config: require("./config") });
-  const keyPair = await provider.useKeyPair({
-    name: provider.config.keyPair.name,
+  const { stage } = provider.config;
+  assert(stage, "missing stage");
+
+  const resources = await createResources({
+    provider,
+    resources: {},
   });
-  const resources = await createResources({ provider, resources: { keyPair } });
 
   return {
     provider,
