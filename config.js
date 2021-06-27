88da3a6cc0e07b6b5e2c15f1a5f122c2c6e678d9
FredericH <frederic.heem@gmail.com>
2021-06-27 15:25:30 +0200

    new infra project

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-/var/folders/f8/r4skslss3c111g5f9p5zhrnw0000gn/T/grucloud-git-b2c611ec-55ef-4e31-b425-93a3b26895999hPO4M/config.js
+const pkg = require("./package.json");
+
+module.exports = ({}) => {
+  return {
+    projectName: "ex-eks-mod",
+    //    formatName: (name, config) => `${name}-${config.projectName}`,
+    formatName: (name, config) => name,
+  };
+};
--- iac.js
+++ iac.js
@@ -1,1 +1,25 @@
-/var/folders/f8/r4skslss3c111g5f9p5zhrnw0000gn/T/grucloud-git-b2c611ec-55ef-4e31-b425-93a3b26895999hPO4M/iac.js
+const { AwsProvider } = require("@grucloud/provider-aws");
+const ModuleAwsVpc = require("@grucloud/module-aws-vpc");
+const ModuleAwsEKS = require("@grucloud/module-aws-eks");
+
+exports.createStack = async ({ config }) => {
+  const provider = AwsProvider({
+    configs: [ModuleAwsVpc.config, ModuleAwsEKS.config, config],
+  });
+
+  const vpcResources = await ModuleAwsVpc.createResources({
+    provider,
+  });
+
+  const eksResources = await ModuleAwsEKS.createResources({
+    provider,
+    resources: vpcResources,
+  });
+
+  return {
+    provider,
+    resources: { vpc: vpcResources, eks: eksResources },
+    hooks: [...ModuleAwsVpc.hooks, ...ModuleAwsEKS.hooks],
+    isProviderUp: () => ModuleAwsEKS.isProviderUp({ resources: eksResources }),
+  };
+};
